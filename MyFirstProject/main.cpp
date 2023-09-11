055c01be724961323b5e690bd88043d6a104278e
CalebCrystal <calwilliamcryst@gmail.com>
2023-09-11 10:50:28 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,19 @@
+/*
+	Caleb Crystal
+	Fall 2023
+	lab 1 introduction to programing
+	create a code that will display hello world 
+	and your favorite color on the screen, 
+	as well as demenstrating basic knowlage about creating projects. 
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World ";
+	std::cout << "Caleb's Favorite color is Blue";
+
+	return 0;
+}
+
